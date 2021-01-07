46f84732c581f1bf93e64b4ba41424c12002a555
Vinayaga Pillai <take2vp@gmail.com>
2021-01-07 17:49:08 +0530

    test arena

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public static Circle _instance;
+
+    Rigidbody2D rb;
+
+    public float jumpSpeed;
+
+    private void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    public void Jump()
+    {
+        rb.velocity = Vector2.up * jumpSpeed;
+    }
+
+}
